7d008b5b9c6d77d3f121a16108efab94ec9dca44
Matt DiDomenico <babsonmatt@gmail.com>
2018-10-13 18:52:46 -0400

    removes unnecessary describe

--- src/components/Table/Table.test.js
+++ src/components/Table/Table.test.js
@@ -40,48 +40,46 @@ function setup() {
   };
 }
 
-describe('components', () => {
-  describe('Table', () => {
-    it('should render self and subcomponents', () => {
-      const { enzymeWrapper } = setup();
+describe('Table', () => {
+  it('should render self and subcomponents', () => {
+    const { enzymeWrapper } = setup();
 
-      // make sure the summary above the table is correct
-      expect(enzymeWrapper.find('div.sub.header').text()).toBe(
-        'Showing 1-2 of 2',
-      );
+    // make sure the summary above the table is correct
+    expect(enzymeWrapper.find('div.sub.header').text()).toBe(
+      'Showing 1-2 of 2',
+    );
 
-      // header and 1 row per company
-      expect(enzymeWrapper.find('tr').length).toBe(3);
+    // header and 1 row per company
+    expect(enzymeWrapper.find('tr').length).toBe(3);
 
-      // make sure we're showing the 'Customer' Tag for the first company
-      expect(
-        enzymeWrapper
-          .find('div.ui.small.label')
-          .first()
-          .text(),
-      ).toBe('Customer');
+    // make sure we're showing the 'Customer' Tag for the first company
+    expect(
+      enzymeWrapper
+        .find('div.ui.small.label')
+        .first()
+        .text(),
+    ).toBe('Customer');
 
-      // make sure we're showing the 'Vendor' Tag for the second company
-      expect(
-        enzymeWrapper
-          .find('div.ui.small.label')
-          .at(1)
-          .text(),
-      ).toBe('Vendor');
+    // make sure we're showing the 'Vendor' Tag for the second company
+    expect(
+      enzymeWrapper
+        .find('div.ui.small.label')
+        .at(1)
+        .text(),
+    ).toBe('Vendor');
 
-      // make sure we're showing the 'Unregistered' Tag for the second company
-      expect(
-        enzymeWrapper
-          .find('div.ui.small.label')
-          .at(2)
-          .text(),
-      ).toBe('Unregistered');
-    });
+    // make sure we're showing the 'Unregistered' Tag for the second company
+    expect(
+      enzymeWrapper
+        .find('div.ui.small.label')
+        .at(2)
+        .text(),
+    ).toBe('Unregistered');
+  });
 
-    it('should call onSearch if the search input has the onChange event fired', () => {
-      const { enzymeWrapper, props } = setup();
-      enzymeWrapper.find('input').simulate('change');
-      expect(props.onSearch.mock.calls.length).toBe(1);
-    });
+  it('should call onSearch if the search input has the onChange event fired', () => {
+    const { enzymeWrapper, props } = setup();
+    enzymeWrapper.find('input').simulate('change');
+    expect(props.onSearch.mock.calls.length).toBe(1);
   });
 });
